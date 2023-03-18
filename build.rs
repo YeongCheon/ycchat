7bb707b7b6402ac124f73be5fa0b57883860568d
YeongCheon Kim <kyc1682@gmail.com>
2023-03-19 00:07:22 +0900

    update protobuf submodule & build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,16 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().compile(&["protobuf/chatService.proto"], &["protobuf"])?;
+    tonic_build::configure().proto_path("protobuf").compile(
+        &[
+            "protobuf/user/user.proto",
+            "protobuf/server/server.proto",
+            "protobuf/server/server_member.proto",
+            "protobuf/category/category.proto",
+            "protobuf/channel/channel.proto",
+            "protobuf/message/message.proto",
+            "protobuf/message/reaction.proto",
+        ],
+        &["protobuf"],
+    )?;
     // tonic_build::compile_protos("protobuf/chatService.proto")?;
     Ok(())
 }
