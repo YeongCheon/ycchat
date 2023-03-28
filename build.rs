4431544073951d9fb648c652556021707d57d37a
YeongCheon Kim <kyc1682@gmail.com>
2023-03-29 00:20:19 +0900

    update build.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,16 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().proto_path("protobuf").compile(
+    tonic_build::configure().compile(
         &[
+            "protobuf/model/attachment.proto",
+            "protobuf/model/category.proto",
+            "protobuf/model/channel.proto",
+            "protobuf/model/message.proto",
+            "protobuf/model/page.proto",
+            "protobuf/model/reaction.proto",
+            "protobuf/model/server.proto",
+            "protobuf/model/server_member.proto",
+            "protobuf/model/user.proto",
+            //---------------------------------------
             "protobuf/user/user.proto",
             "protobuf/server/server.proto",
             "protobuf/server/server_member.proto",
